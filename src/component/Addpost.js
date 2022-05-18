28eeb95351c459375ff95edcadba803976da0dac
ansalnas <ansalnashajahan98@gmail.com>
2022-05-18 13:34:00 +0530

    sstudent

---
+++ src/component/Addpost.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function Addpost() {
+  return (
+    <div>
+        <div ClassName="container">
+    <div ClassName="row">
+        <div ClassName="col col-12 col-lg-12 col-md-12 col-xl-12 col-xxl-12">
+            <div ClassName="container">
+                <div ClassName="row">
+                    <div ClassName="col-12 col-lg-12 col-md-12 col-xl-12 col-xxl-12">
+                        <label for="" ClassName="form-label">title</label>
+                        <input type="text" ClassName="form-control"/>
+                    </div>
+                    <div ClassName="col-12 col-lg-12 col-md-12 col-xl-12 col-xxl-12">
+                    <textarea name="" id="" cols="30" rows="10" ClassName="form-control">message</textarea>
+                
+                        
+                    </div>
+                    <div ClassName="col-12 col-lg-12 col-md-12 col-xl-12 col-xxl-12">
+                        <button ClassName="btn btn-success">submit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    </div>
+  )
+}
+
+export default Addpost
